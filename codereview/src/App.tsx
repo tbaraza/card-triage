978e11985f9f9b802079dc07d42683463ba9d1c2
tbaraza <tonidahnjeli@gmail.com>
2022-06-27 14:54:21 +0200

    Migrated to Typescript

--- codereview/src/App.tsx
+++ codereview/src/App.tsx
@@ -1,58 +1,63 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
   const [cards, setCards] = useState([]);
-  const pendingCards = cards.filter((card) => card.status === 'PENDING')
-  const rejectedCards = cards.filter((card) => card.status === 'REJECTED')
-  const doneCards = cards.filter((card) => card.status === 'DONE')
+  const pendingCards = cards.filter((card: any) => card.status === 'PENDING')
+  const rejectedCards = cards.filter((card: any) => card.status === 'REJECTED')
+  const doneCards = cards.filter((card: any) => card.status === 'DONE')
 
   useEffect(() => {
     fetch('http://localhost:3000/cards')
       .then(resp => resp.json())
       .then(data => {
-        data.forEach((idx, card) => card.display = true);
+        data.forEach((card: any, idx: number) => card.display = true);
         setCards(data);
       })
   }, [])
 
-  function doneCard(card) {
+  function doneCard(card: any) {
     for (let i = 0; i < cards.length; i++) {
-      if (cards[i] === card) {
-        cards[i].status = 'DONE';
+      const current: any = cards[i];
+      if (current === card) {
+        current.status = 'DONE';
       }
     }
     setCards([...cards])
   }
 
-  function rejectCard(card) {
+  function rejectCard(card: any) {
     for (let i = 0; i < cards.length; i++) {
-      if (cards[i] === card) {
-        cards[i].status = 'REJECTED';
+      const current: any = cards[i];
+      if (current === card) {
+        current.status = 'REJECTED';
       }
     }
     setCards([...cards])
   }
 
-  function searchCards(value) {
+  function searchCards(value: any) {
     for (let i = 0; i < cards.length; i++) {
-      cards[i].display = cards[i].patient_name.match(value)
+      const current: any = cards[i];
+      current.display = current.patient_name.match(value)
     }
     setCards([...cards])
   }
 
-  function filterArrhythmias(value) {
+  function filterArrhythmias(value: any) {
     for (let i = 0; i < cards.length; i++) {
-      cards[i].display = cards[i].arrhythmias.find(name => name === value)
+      const current: any = cards[i];
+      current.display = current.arrhythmias.find((name: string) => name === value)
     }
     setCards([...cards])
   }
 
   return (
-    <div class="app">
+    <div className="app">
       <div>
         <h1 style={{ margin: 0, float: 'left', marginRight: '20px' }}>Patient triage app</h1>
-        <div class="form">
+        <div className="form">
           <input type="text" onChange={(evt) => searchCards(evt.target.value)} />
           <select onChange={(evt) => filterArrhythmias(evt.target.value)}>
             <option value="">--</option>
@@ -64,49 +69,49 @@ function App() {
           </select>
         </div>
       </div>
-      <div class="cols">
-        <div class="col">
-          <p id="pending-title" align="center">PENDING</p>
+      <div className="cols">
+        <div className="col">
+          <p id="pending-title" style={{textAlign: 'center'}}>PENDING</p>
           <hr />
-          {pendingCards.map((card, idx) => (
-            <div class="card" style={{ display: card.display ? 'block' : 'none' }}>
+          {pendingCards.map((card: any, idx) => (
+            <div className="card" style={{ display: card.display ? 'block' : 'none' }}>
               <h2>{card.patient_name}</h2>
               <p>{card.arrhythmias.join(',')}</p>
               <cite>{new Date(card.creation_date).toLocaleString()}</cite>
 
               <div>
-                <div class="btn" onClick={() => doneCard(card)}>DONE</div>
-                <div class="btn" onClick={() => rejectCard(card)}>REJECT</div>
+                <div className="btn" onClick={() => doneCard(card)}>DONE</div>
+                <div className="btn" onClick={() => rejectCard(card)}>REJECT</div>
               </div>
             </div>
           ))}
         </div>
-        <div class="col">
-          <p id="rejected-title" align="center">REJECTED</p>
+        <div className="col">
+          <p id="rejected-title"style={{textAlign: 'center'}}>REJECTED</p>
           <hr />
-          {rejectedCards.map((card, idx) => (
-            <div class="card" style={{ display: card.display ? 'block' : 'none' }}>
+          {rejectedCards.map((card: any, idx) => (
+            <div className="card" style={{ display: card.display ? 'block' : 'none' }}>
               <h2>{card.patient_name}</h2>
               <p>{card.arrhythmias.join(',')}</p>
               <cite>{new Date(card.created_date).toLocaleString()}</cite>
 
               <div>
-                <div class="btn" onClick={() => doneCard(card)}>DONE</div>
+                <div className="btn" onClick={() => doneCard(card)}>DONE</div>
               </div>
             </div>
           ))}
         </div>
-        <div class="col">
-          <p id="done-title" align="center">DONE</p>
+        <div className="col">
+          <p id="done-title"style={{textAlign: 'center'}}>DONE</p>
           <hr />
-          {doneCards.map((card, idx) => (
-            <div class="card" style={{ display: card.display ? 'block' : 'none' }}>
+          {doneCards.map((card: any, idx) => (
+            <div className="card" style={{ display: card.display ? 'block' : 'none' }}>
               <h2>{card.patient_name}</h2>
               <p>{card.arrhythmias.join(',')}</p>
               <cite>{new Date(card.created_date).toLocaleString()}</cite>
 
               <div>
-                <div class="btn" onClick={() => rejectCard(card)}>REJECT</div>
+                <div className="btn" onClick={() => rejectCard(card)}>REJECT</div>
               </div>
             </div>
           ))}
