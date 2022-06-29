1b7f784213074c928d4f4cc821243422fdc0faf2
tbaraza <tonidahnjeli@gmail.com>
2022-06-29 10:54:01 +0200

    Code cleanup

--- codereview/src/store.ts
+++ codereview/src/store.ts
@@ -2,13 +2,6 @@ import { configureStore } from '@reduxjs/toolkit'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import patientFlowReducer from './patient-flow/services/patientFlowSlice'
 
-
-declare global {
-    interface Window {
-        __REDUX_DEVTOOLS_EXTENSION__: () => {};
-    }
-  }
-
 export const store = configureStore({
   reducer: {
     patientFlow: patientFlowReducer
@@ -17,7 +10,7 @@ export const store = configureStore({
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+
 export type AppDispatch = typeof store.dispatch
 
 // maybe move these to a hooks files
